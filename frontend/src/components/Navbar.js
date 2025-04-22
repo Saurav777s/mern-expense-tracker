a9ab8c19f53ce029aa23937feafb422e71179f35
Saurav@777s <sauravljslm@gmail.com>
2025-04-22 11:26:19 +0530

    Fix: Removed unused Link import to fix Netlify build

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+
 import { ThemeContext } from '../context/ThemeContext';
 
 const Navbar = () => {
